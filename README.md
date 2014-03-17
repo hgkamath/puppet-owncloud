c4d961b1c4eefdd7bf5afa063091451e174a07f7
Ganapathi Kamath <kamath@sirius.localdomain>
2014-03-17 02:55:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# puppet-owncloud
+
+This puppet module is to deploy owncloud in fedora or fedora-like distributions.  I had trouble with other puppet deployments because paths and otherthings can be different. 
+
+Goals
+* to have a separate owncloud server that worked on a fedora based system
+
+Troubles that need ironing out, manually for now.
+
---
+++ manifests/server/params.pp
@@ -0,0 +1,34 @@
+# Class: owncloud::server::params
+#
+# This class defines default parameters used by the main module class phpmyadmin
+# Operating Systems differences in names and paths are addressed here
+#
+# == Variables
+#
+# Refer to owncloud::server class for the variables defined here.
+#
+# == Usage
+#
+# This class is not intended to be used directly.
+# It may be imported or inherited by other classes
+#
+class owncloud::server::params {
+
+  ### Application related parameters
+
+  $packages = $::operatingsystem ? {
+    default => 'owncloud'
+  }
+
+  $path = '/usr/share/owncloud'
+  $data_dir = '/var/lib/owncloud'
+
+  $user           = 'owncloud'
+  $passwordsalt   = undef
+  $mysql_database = 'owncloud'
+  $mysql_user     = 'owncloud'
+  $mysql_host     = 'localhost'
+
+  $enabled = true
+
+}
